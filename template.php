98e2ba0890b41aed0928fdf8f8b1493e115f9913
nata <korovina-nataliya@mail.ru>
2018-11-15 15:00:25 +0300

    working script

--- template.php
+++ template.php
@@ -1,3 +1,38 @@
+<link rel="stylesheet" href="style.css" type="text/css"/>
 <form method="post">
     <input type="submit" value="<?= $control->getButtonTitle() ?>">
 </form>
+
+<table>
+    <?php foreach($work['table'] as $row):?>
+        <tr>
+            <?php foreach($row as $col):?>
+                <td>
+                    <?= $col; ?>
+                </td>
+            <?php endforeach; ?>
+        </tr>
+    <?php endforeach; ?>
+    <tr>
+        <td>Total:</td>
+        <td><?= $work['sum'] ?></td>
+    </tr>
+</table>
+
+<div>
+    <table>
+        <?php foreach($rest['table'] as $row):?>
+            <tr>
+                <?php foreach($row as $col):?>
+                    <td>
+                        <?= $col; ?>
+                    </td>
+                <?php endforeach; ?>
+            </tr>
+        <?php endforeach; ?>
+        <tr>
+            <td>Total:</td>
+            <td><?= $rest['sum'] ?></td>
+        </tr>
+    </table>
+</div>
